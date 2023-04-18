90e3a8ed032839e76c61b7f33d1de6ae61249c80
abndetto <121995170+abndetto@users.noreply.github.com>
2023-04-18 10:22:05 -0400

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,49 @@
+document.getElementsByClassName("foodImg")[0].addEventListener("click", function(){
+    let altMessage = this.alt;
+    alert(altMessage);
+})
+
+document.getElementsByClassName("foodImg")[1].addEventListener("click", function(){
+    let altMessage = this.alt;
+    alert(altMessage);
+})
+
+document.getElementsByClassName("foodImg")[2].addEventListener("click", function(){
+    let altMessage = this.alt;
+    alert(altMessage);
+})
+
+document.getElementsByClassName("foodImg")[3].addEventListener("click", function(){
+    let altMessage = this.alt;
+    alert(altMessage);
+})
+
+document.getElementsByClassName("foodImg")[4].addEventListener("click", function(){
+    let altMessage = this.alt;
+    alert(altMessage);
+})
+
+document.getElementsByClassName("foodImg")[5].addEventListener("click", function(){
+    let altMessage = this.alt;
+    alert(altMessage);
+})
+
+document.getElementsByClassName("foodImg")[0].addEventListener("click", function(){
+    let altMessage = this.alt;
+    alert(altMessage);
+})
+
+document.getElementsByClassName("foodImg")[6].addEventListener("click", function(){
+    let altMessage = this.alt;
+    alert(altMessage);
+})
+
+document.getElementsByClassName("foodImg")[7].addEventListener("click", function(){
+    let altMessage = this.alt;
+    alert(altMessage);
+})
+
+document.getElementsByClassName("foodImg")[8].addEventListener("click", function(){
+    let altMessage = this.alt;
+    alert(altMessage);
+})
